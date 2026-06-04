27aad37d295b9fec45324c247ca26005a3cf32aa
RAHALOmar <159546485+RAHALOmar@users.noreply.github.com>
2026-06-04 16:24:19 +0200

    removed time since it is disturbing on the terminal

--- src/utils/logger.py
+++ src/utils/logger.py
@@ -2,7 +2,7 @@ import logging
 
 logging.basicConfig(
     level=logging.INFO,
-    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    format="%(name)s - %(levelname)s - %(message)s",
     handlers=[
         logging.StreamHandler()
     ],  # maybe add a FileHandler in the future : logging.FileHandler("log_datetime.log")
