f0efbeed8c74c49c32252c539dec9d357b4f6fe9
RAHALOmar <159546485+RAHALOmar@users.noreply.github.com>
2026-06-05 14:40:00 +0200

    added this script to create the needed tables from the weather_schema.sql file

---
+++ scripts/create_tables.py
@@ -0,0 +1,36 @@
+"""
+script to create the tables in the PostgreSQL database
+using the schema defined in weather_schema.sql
+"""
+
+import psycopg2
+
+from src.utils.config import Config
+from src.utils.logger import logger
+
+
+def create_tables():
+    with psycopg2.connect(
+        host=Config.DB_HOST,
+        port=Config.DB_PORT,
+        dbname=Config.DB_NAME,
+        user=Config.DB_USER,
+        password=Config.DB_PASSWORD,
+    ) as conn:
+        with conn.cursor() as cur:
+
+            with open(
+                "database/schemas/weather_schema.sql", "r", encoding="utf-8"
+            ) as f:
+
+                schema = f.read()
+
+            cur.execute(schema)
+
+        conn.commit()
+
+    logger.info("Tables created successfully.")
+
+
+if __name__ == "__main__":
+    create_tables()
