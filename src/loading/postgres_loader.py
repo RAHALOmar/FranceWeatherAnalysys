720b6e5a50eecd73931fe5d31d322bb8737b9d5d
RAHALOmar <159546485+RAHALOmar@users.noreply.github.com>
2026-06-05 14:37:30 +0200

    added connection and row insertion to the postgres database

--- src/loading/postgres_loader.py
+++ src/loading/postgres_loader.py
@@ -1,10 +1,65 @@
+import psycopg2
+
+from ..utils.config import Config
+from ..utils.logger import logger
+
+
 class PostgresLoader:
 
     def __init__(self):
-        pass
+        self.connection = None
 
     def connect(self):
-        pass
+        try:
+            self.connection = psycopg2.connect(
+                host=Config.DB_HOST,
+                port=Config.DB_PORT,
+                database=Config.DB_NAME,
+                user=Config.DB_USER,
+                password=Config.DB_PASSWORD,
+            )
+            logger.info("Connected to PostgreSQL database.")
+        except Exception as e:
+            logger.error(f"Error connecting to PostgreSQL database: {e}")
+            raise
 
-    def insert_weather(self, weather_data):
-        pass
+    def insert_weather(self, weather_data: dict):
+        if self.connection is None:
+            self.connect()
+        try:
+            with self.connection.cursor() as cursor:
+                insert_query = """
+                    INSERT INTO weather_raw (
+                        country_code, city, 
+                        temperature, feels_like, temperature_min, temperature_max,
+                        humidity, pressure, 
+                        weather_main, weather_description, 
+                        wind_speed,
+                        cloudiness, 
+                        extraction_time
+                    ) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+                """
+                cursor.execute(
+                    insert_query,
+                    (
+                        weather_data["country_code"],
+                        weather_data["city"],
+                        weather_data["temperature"],
+                        weather_data["feels_like"],
+                        weather_data["temperature_min"],
+                        weather_data["temperature_max"],
+                        weather_data["humidity"],
+                        weather_data["pressure"],
+                        weather_data["weather_main"],
+                        weather_data["weather_description"],
+                        weather_data["wind_speed"],
+                        weather_data["cloudiness"],
+                        weather_data["extraction_time"],
+                    ),
+                )
+            self.connection.commit()
+            logger.info("Weather data inserted successfully.")
+        except Exception as e:
+            logger.error(f"Error inserting weather data: {e}")
+            self.connection.rollback()
+            raise
