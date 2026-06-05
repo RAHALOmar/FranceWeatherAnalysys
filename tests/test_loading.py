44e89998e4a78b7c3c08632653e4f01e684496c7
RAHALOmar <159546485+RAHALOmar@users.noreply.github.com>
2026-06-05 14:38:23 +0200

    added loading testing and verfication

---
+++ tests/test_loading.py
@@ -0,0 +1,9 @@
+from src.utils.config import Config
+from src.extraction.weather_api import WeatherAPI
+from src.loading.postgres_loader import PostgresLoader
+
+api = WeatherAPI(api_key=Config.OPENWEATHER_API_KEY)
+weather_data = api.get_limited_weather_data(country="FR", city="Brest")
+loader = PostgresLoader()
+# loader.execute_db("database\schemas\weather_schema.sql")
+loader.insert_weather(weather_data)
