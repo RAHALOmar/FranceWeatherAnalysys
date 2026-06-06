15874a4804e55dafb1d628aeb6781da215b9605e
RAHALOmar <159546485+RAHALOmar@users.noreply.github.com>
2026-06-06 15:11:44 +0200

    added a new sql file for the new processed data

---
+++ database/schemas/weather_clean.sql
@@ -0,0 +1,25 @@
+CREATE TABLE weather_clean (
+    id SERIAL PRIMARY KEY,
+
+    country_code VARCHAR(2) NOT NULL,
+    city VARCHAR(100) NOT NULL,
+
+    temperature FLOAT NOT NULL,
+    feels_like FLOAT,
+    temperature_min FLOAT NOT NULL,
+    temperature_max FLOAT NOT NULL,
+
+    humidity INTEGER,
+    pressure INTEGER,
+
+    weather_main VARCHAR(100),
+    weather_description VARCHAR(255),
+
+    wind_speed FLOAT,
+
+    cloudiness INTEGER,
+
+    temperature_category VARCHAR(20),
+
+    extraction_time TIMESTAMPTZ NOT NULL
+);
