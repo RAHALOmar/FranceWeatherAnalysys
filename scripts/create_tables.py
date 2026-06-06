d50cc572504b136cf098acd80c0a2f7d94ead784
RAHALOmar <159546485+RAHALOmar@users.noreply.github.com>
2026-06-06 15:10:29 +0200

    added variable path to create tables in other files too

--- scripts/create_tables.py
+++ scripts/create_tables.py
@@ -12,7 +12,13 @@ from src.utils.config import Config
 from src.utils.logger import logger
 
 
-def create_tables():
+def create_tables(path="database/schemas/weather_schema.sql"):
+    """
+    Creates tables in the PostgreSQL database using the schema defined in the provided SQL file.
+    Args:
+        path (str): The file path to the SQL schema file. Default is "database/schemas/weather_schema.sql".
+
+    """
     with psycopg2.connect(
         host=Config.DB_HOST,
         port=Config.DB_PORT,
@@ -22,9 +28,7 @@ def create_tables():
     ) as conn:
         with conn.cursor() as cur:
 
-            with open(
-                "database/schemas/weather_schema.sql", "r", encoding="utf-8"
-            ) as f:
+            with open(path, "r", encoding="utf-8") as f:
 
                 schema = f.read()
 
@@ -36,4 +40,5 @@ def create_tables():
 
 
 if __name__ == "__main__":
-    create_tables()
+    create_tables(path="database/schemas/weather_schema.sql")
+    create_tables(path="database/schemas/weather_clean.sql")
