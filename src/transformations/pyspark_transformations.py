2497e26d3d7bfab6061cbdc92e0b9e17790b34cc
RAHALOmar <159546485+RAHALOmar@users.noreply.github.com>
2026-06-12 10:09:16 +0200

    added a commet explaining why i have more colums in the weather_clean table than the weather_raw table wich was not logical

--- src/transformations/pyspark_transformations.py
+++ src/transformations/pyspark_transformations.py
@@ -39,7 +39,7 @@ class WeatherTransformer:
 
     def clean_weather_data(self, df):
 
-        # Remove duplicates
+        # Remove duplicates from the raw data
         df = df.dropDuplicates()
 
         # Keep only realistic temperatures
@@ -79,6 +79,16 @@ class WeatherTransformer:
         clean_df = self.clean_weather_data(raw_df)
         clean_df = clean_df.drop("id")
 
+        # this will add the cleaned data to the table but will not check if the data already
+        # exists in the table so if i run the tests.test_transformations multiple times it
+        # will add the same data multiple times to the table. so basically we will have duplicates
+        # in the weather_clean table but we will not have duplicates in the weather_raw table
+        # because we are not adding data to it in this transformation.
+        # so if we want to avoid duplicates in the weather_clean table we can either add a unique
+        # constraint on the city and extraction_time columns or we can check if the data already
+        # exists in the table before adding it. but for now i will just add the data to the table
+        # without checking for duplicates because it is not a big deal for this project and
+        # it will make the code simpler.
         self.write_clean_weather(clean_df)
 
         logger.info(
